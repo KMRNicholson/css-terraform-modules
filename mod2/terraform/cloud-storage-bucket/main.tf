cae3773933bfbe9fd2c7b51c015bc793b184a069
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-10-03 19:46:45 +0300

    feat[mod2-2]: add terraform file for provisioning storage bucket

---
+++ mod2/terraform/cloud-storage-bucket/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = "tf_mod2_empty_bucket"
+}
+
+variable "folder_name" {
+  type        = string
+  default     = "empty_folder"
+}
+
+provider "google" {
+  project     = "css-kohdynicholson-2023"
+  region      = "europe-central2"
+  zone        = "europe-central2-a"
+}
+
+resource "google_storage_bucket" "empty_bucket" {
+  name        = "${var.bucket_name}"
+  location    = "EU"
+  uniform_bucket_level_access = true
+  force_destroy = true
+}
+
+resource "google_storage_bucket_object" "empty_folder" {
+  name        = "${var.folder_name}/" # folder name should end with '/'
+  content     = "This content does not matter"
+  bucket      = "${google_storage_bucket.empty_bucket.name}"
+}
